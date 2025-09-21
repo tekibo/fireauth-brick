b2c13e173212cbdd22bb605a250eaef2ac5acf81
tekibo <tekibohelp@gmail.com>
2025-09-21 13:53:29 +0530

    _

--- hooks/pre_gen.dart
+++ hooks/pre_gen.dart
@@ -1,7 +1,7 @@
 import 'package:mason/mason.dart';
 
 void run(HookContext context) {
-  final scopes = context.vars['scopes'] as List;
-  final formattedScopes = scopes.map((s) => "'$s'").join(', ');
-  context.vars['scopes'] = formattedScopes;
+  // final scopes = context.vars['scopes'] as List;
+  // final formattedScopes = scopes.map((s) => "'$s'").join(', ');
+  // context.vars['scopes'] = formattedScopes;
 }
