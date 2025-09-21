9819e93a47c9b8d95df108e45dc03fd5982a74e6
tekibo <tekibohelp@gmail.com>
2025-09-21 13:44:14 +0530

    _

--- hooks/pre_gen.dart
+++ hooks/pre_gen.dart
@@ -4,7 +4,7 @@ void run(HookContext context) {
   final scopes = context.vars['scopes'];
   final newScopes = [];
   for (var scope in scopes) {
-    newScopes.add(scope.toString().replaceAll('^&#x2F;', '\/').toString());
+    newScopes.add(Uri.decodeComponent(scope.toString()));
   }
   context.vars['scopes'] = newScopes;
 }
