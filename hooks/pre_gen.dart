056c8d18778f0ea723838fae10c08ef0718e39d9
tekibo <tekibohelp@gmail.com>
2025-09-21 13:43:11 +0530

    _

--- hooks/pre_gen.dart
+++ hooks/pre_gen.dart
@@ -2,8 +2,9 @@ import 'package:mason/mason.dart';
 
 void run(HookContext context) {
   final scopes = context.vars['scopes'];
-  scopes.forEach(
-    (scope) => scope.toString().replaceAll('^&#x2F;', '\/').toString(),
-  );
-  context.vars['scopes'] = scopes;
+  final newScopes = [];
+  for (var scope in scopes) {
+    newScopes.add(scope.toString().replaceAll('^&#x2F;', '\/').toString());
+  }
+  context.vars['scopes'] = newScopes;
 }
