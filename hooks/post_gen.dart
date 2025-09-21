a68b3aa3b075267e29ddcd6e17fa853aaa26c0e1
tekibo <tekibohelp@gmail.com>
2025-09-21 14:07:17 +0530

    _

--- hooks/post_gen.dart
+++ hooks/post_gen.dart
@@ -2,17 +2,27 @@ import 'dart:io';
 import 'package:mason/mason.dart';
 
 Future<void> run(HookContext context) async {
-  // final progress = context.logger.progress('Installing packages');
+  final progress = context.logger.progress('Installing packages');
 
-  // final packages = ['firebase_auth'];
+  final packages = ['firebase_auth'];
 
-  // if (context.vars['useCredMgr'] == 'true') {
-  //   packages.add('credential_manager');
-  // } else {
-  //   packages.add('google_sign_in');
-  // }
+  if (context.vars['useCredMgr'] == 'true') {
+    packages.add('credential_manager');
+  } else {
+    packages.add('google_sign_in');
+  }
 
-  // await Process.run('flutter', ['pub', 'add', ...packages]);
+  final result = await Process.run('flutter', [
+    'pub',
+    'add',
+    ...packages,
+  ], runInShell: true);
 
-  // progress.complete();
+  if (result.exitCode != 0) {
+    context.logger.err('Failed to install packages:\n${result.stderr}');
+  } else {
+    context.logger.info(result.stdout);
+  }
+
+  progress.complete();
 }
